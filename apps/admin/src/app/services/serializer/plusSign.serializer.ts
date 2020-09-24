f9a0157f991a50e28ba6c1bedf4a4ed8d4b19bef
Lascario Pacheco <lascariopacheco@hotmail.com>
2020-09-24 05:41:47 -0400

    build: upgraded to ng10

--- apps/admin/src/app/services/serializer/plusSign.serializer.ts
+++ apps/admin/src/app/services/serializer/plusSign.serializer.ts
@@ -1,5 +1,7 @@
 import { DefaultUrlSerializer, UrlSerializer, UrlTree } from '@angular/router';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class PlusSignSerializer implements UrlSerializer {
   private defaultUrlSerializer: DefaultUrlSerializer = new DefaultUrlSerializer();
 
