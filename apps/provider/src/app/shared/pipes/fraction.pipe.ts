5e727bf3ded1b22369036d2e7ba34499d41a7e37
Lascario Pacheco <lascariopacheco@hotmail.com>
2021-01-21 14:32:10 -0300

    fix: adjusted fractional value pipe (#75)

--- apps/provider/src/app/shared/pipes/fraction.pipe.ts
+++ apps/provider/src/app/shared/pipes/fraction.pipe.ts
@@ -2,9 +2,54 @@ import { Pipe, PipeTransform } from '@angular/core'
 
 @Pipe({ name: 'fraction' })
 export class FractionPipe implements PipeTransform {
+  private fractionDeviation = 0.005
+  private hardCodedFractions = [
+    {
+      top: 1 / 2 + this.fractionDeviation,
+      bottom: 1 / 2 - this.fractionDeviation,
+      displayValue: '1/2'
+    },
+    {
+      top: 1 / 3 + this.fractionDeviation,
+      bottom: 1 / 3 - this.fractionDeviation,
+      displayValue: '1/3'
+    },
+    {
+      top: 1 / 4 + this.fractionDeviation,
+      bottom: 1 / 4 - this.fractionDeviation,
+      displayValue: '1/4'
+    },
+    {
+      top: 1 / 6 + this.fractionDeviation,
+      bottom: 1 / 6 - this.fractionDeviation,
+      displayValue: '1/6'
+    },
+    {
+      top: 1 / 8 + this.fractionDeviation,
+      bottom: 1 / 8 - this.fractionDeviation,
+      displayValue: '1/8'
+    },
+    {
+      top: 1 / 9 + this.fractionDeviation,
+      bottom: 1 / 9 - this.fractionDeviation,
+      displayValue: '1/9'
+    },
+    {
+      top: 2 / 3 + this.fractionDeviation,
+      bottom: 2 / 3 - this.fractionDeviation,
+      displayValue: '2/3'
+    }
+  ]
+
   public transform(value: number): string {
     if (!value) {
       return ''
+    }
+
+    const hardCodedMatch = this.getHardCodedMatch(value)
+
+    if (hardCodedMatch) {
+      return hardCodedMatch
     }
 
     const rawNumber = value.toString().split('.')
@@ -14,6 +59,15 @@ export class FractionPipe implements PipeTransform {
     }
 
     const fraction = Number(`0.${rawNumber[1]}`)
+
+    const fractionHardCodedMatch = this.getHardCodedMatch(fraction)
+
+    if (fractionHardCodedMatch) {
+      return `${
+        Number(rawNumber[0]) ? rawNumber[0] + ' ' : ''
+      }${fractionHardCodedMatch}`
+    }
+
     const len = fraction.toString().length - 2
 
     let denominator = Math.pow(10, len)
@@ -24,7 +78,9 @@ export class FractionPipe implements PipeTransform {
     numerator /= divisor
     denominator /= divisor
 
-    return `${rawNumber[0]} ${numerator.toFixed()}/${denominator.toFixed()}`
+    return `${
+      Number(rawNumber[0]) ? rawNumber[0] + ' ' : ''
+    }${numerator.toFixed()}/${denominator.toFixed()}`
   }
 
   private greatestCommonDenom(a, b): number {
@@ -36,5 +92,17 @@ export class FractionPipe implements PipeTransform {
     b = parseInt(b, 10)
 
     return this.greatestCommonDenom(b, a % b)
+  }
+
+  private getHardCodedMatch(value: number): string {
+    const match = this.hardCodedFractions.find(
+      (fractionObj) => fractionObj.top >= value && fractionObj.bottom <= value
+    )
+
+    if (match) {
+      return match.displayValue
+    }
+
+    return
   }
 }
