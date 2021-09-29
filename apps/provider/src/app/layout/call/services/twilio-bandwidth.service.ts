31edbcc84d84ed006e3a2aa81c604fc3d34603ea
Lascario Pacheco <zcyonzcyon@gmail.com>
2021-09-29 19:55:50 -0300

    feat: added support for camera toggling message [IDEA-929]

--- apps/provider/src/app/layout/call/services/twilio-bandwidth.service.ts
+++ apps/provider/src/app/layout/call/services/twilio-bandwidth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core'
 import { ContextService, LoggingService } from '@app/service'
-import { ConnectionStats } from '@coachcare/sdk'
+import {
+  ConnectionStats,
+  TwilioDataMessage,
+  TwilioDataMessageType
+} from '@coachcare/sdk'
 import { Subject } from 'rxjs'
 import { TwilioRoomMonitor } from '@coachcare/sdk'
-import { LocalDataTrack, Room } from 'twilio-video'
+import { LocalDataTrack, Room, StatsReport } from 'twilio-video'
+import { Store } from '@ngrx/store'
+import {
+  callSelector,
+  CallState,
+  DisableCurrentUserCamera,
+  EnableCurrentUserCamera
+} from '@app/layout/store/call'
+import { sleep } from '@app/shared/utils'
 
 @Injectable()
 export class TwilioBandwidthService {
@@ -18,15 +30,22 @@ export class TwilioBandwidthService {
   private dataTrackPublished: any = {}
   private roomMonitor: TwilioRoomMonitor
   private subscriptions = []
+  private monitorPulse = 0
   private monitorInterval: any
   private monitorIntervalTimeout = 1000
   private readonly REMOTE_CONNECTIVITY_SKIPS: number = 5
+  private callState: CallState
 
   constructor(
     private context: ContextService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    private store: Store<CallState>
   ) {
     this.onMessageReceived = this.onMessageReceived.bind(this)
+
+    this.store
+      .select(callSelector)
+      .subscribe((state) => (this.callState = state))
   }
 
   public monitorRoom(room: Room): void {
@@ -47,16 +66,32 @@ export class TwilioBandwidthService {
     ]
 
     this.monitorInterval = setInterval(async () => {
-      this.roomMonitor.monitorRoomHandler(await this.currentRoom.getStats())
+      ++this.monitorPulse
+      const stats = await this.currentRoom.getStats()
+      this.checkRoomStability(stats)
+      this.roomMonitor.monitorRoomHandler(stats)
     }, this.monitorIntervalTimeout)
   }
 
-  public onMessageReceived(raw: string): void {
+  public async onMessageReceived(raw: string): Promise<void> {
     this.roomMonitor.onMessageReceived(raw)
-    const message = JSON.parse(raw)
+    const message: TwilioDataMessage = JSON.parse(raw)
 
-    if (message.type === 3 || message.type === 4) {
-      this.listenToVideoDataTrack = true
+    switch (message.type) {
+      case TwilioDataMessageType.MOB_CAM_OFF:
+      case TwilioDataMessageType.MOB_CAM_ON:
+        this.listenToVideoDataTrack = true
+        break
+
+      case TwilioDataMessageType.TOGGLE_CAM:
+        if (!this.callState.isCameraEnabled) {
+          return
+        }
+        // toggle the cam programatically
+        this.store.dispatch(new DisableCurrentUserCamera())
+        await sleep(1)
+        this.store.dispatch(new EnableCurrentUserCamera())
+        break
     }
   }
 
@@ -76,6 +111,10 @@ export class TwilioBandwidthService {
     this.roomMonitor.sendMobileOnMessage()
   }
 
+  public sendToggleCameraMessage(): void {
+    this.roomMonitor.sendToggleCameraMessage()
+  }
+
   public setRemoteConnSkips(): void {
     this.currentRemoteConnSkips = this.REMOTE_CONNECTIVITY_SKIPS
   }
@@ -87,8 +126,21 @@ export class TwilioBandwidthService {
 
     this.listenToVideoDataTrack = false
     clearInterval(this.monitorInterval)
+    this.monitorPulse = 0
     this.roomMonitor.stopMonitoringRoom()
     this.unsubscribe()
+  }
+
+  private checkRoomStability(stats: StatsReport[]): void {
+    const single = stats[0]
+
+    if (single?.remoteVideoTrackStats?.length < 2) {
+      return
+    }
+
+    if (this.monitorPulse > 0 && this.monitorPulse % 10 === 0) {
+      this.sendToggleCameraMessage()
+    }
   }
 
   private sendMessage(data: string): void {
