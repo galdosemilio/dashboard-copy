da7ad89a1190a2ce88eefb3b91cbb4dbd8f6ed4b
zhang <zhangxinei@outlook.com>
2021-11-16 10:46:57 -0500

    fix: modify form removable submissions setting [FRON-2463]

--- apps/provider/src/app/dashboard/library/forms/forms/form.form.component.ts
+++ apps/provider/src/app/dashboard/library/forms/forms/form.form.component.ts
@@ -2,11 +2,14 @@ import { Component, forwardRef, Input, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { ActivatedRoute } from '@angular/router'
 import { Form } from '@app/dashboard/library/forms/models'
+import { ContextService, NotifierService } from '@app/service'
 import { BINDFORM_TOKEN } from '@app/shared'
+import { Feedback } from '@coachcare/sdk'
 
 @Component({
   selector: 'app-library-form-form',
   templateUrl: './form.form.component.html',
+  styleUrls: ['./form.form.component.scss'],
   providers: [
     {
       provide: BINDFORM_TOKEN,
@@ -16,6 +19,11 @@ import { BINDFORM_TOKEN } from '@app/shared'
 })
 export class FormFormComponent implements OnInit {
   public readonly = false
+  public removableSubmissionsStatus:
+    | 'pending'
+    | 'submitting'
+    | 'submitted'
+    | 'error' = 'pending'
 
   @Input()
   edit: boolean
@@ -51,7 +59,13 @@ export class FormFormComponent implements OnInit {
 
   private _form: Form
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute) {
+  constructor(
+    private context: ContextService,
+    private feedback: Feedback,
+    private formBuilder: FormBuilder,
+    private notifier: NotifierService,
+    private route: ActivatedRoute
+  ) {
     this.createForm()
   }
 
@@ -77,5 +91,31 @@ export class FormFormComponent implements OnInit {
         Validators.required
       ]
     })
+  }
+
+  public async requestChangeRemovableSubmissions(): Promise<void> {
+    try {
+      this.removableSubmissionsStatus = 'submitting'
+
+      await this.feedback.sendFeedback({
+        title: 'Request to modify form Removable Submissions setting',
+        description: `This request was automatically generated. ${
+          this.context.user.firstName
+        } ${this.context.user.lastName} (ID: ${
+          this.context.user.id
+        }) requests that the Removable Submissions setting for form “${
+          this._form.name
+        }” (ID: ${this._form.id}) be changed from “${
+          this._form.removableSubmissions ? 'Yes' : 'No'
+        }” to “${
+          !this._form.removableSubmissions ? 'Yes' : 'No'
+        }”. Support team, please make this adjustment and update this support ticket when the change is complete.`
+      })
+
+      this.removableSubmissionsStatus = 'submitted'
+    } catch (error) {
+      this.notifier.error(error)
+      this.removableSubmissionsStatus = 'error'
+    }
   }
 }
