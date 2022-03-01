e0857c864a07cb6ebcb7d75b0498ac9fbb1bf73f
Lascario Pacheco <zcyonzcyon@gmail.com>
2022-03-01 12:16:24 -0300

    fix: enabled messages custom report for inhealth [FRON-2636]

--- apps/provider/src/app/config/section/inhealth/inhealth.section.config.ts
+++ apps/provider/src/app/config/section/inhealth/inhealth.section.config.ts
@@ -1,7 +1,11 @@
+import { SidenavOptions } from '../consts'
 import { SectionConfigDetails } from '../models/section.details'
 
 export const InhealthSectionConfig: SectionConfigDetails = {
   CLINIC_LISTING: {
     SHOW_CLINIC_CREATE_BUTTON: true
+  },
+  SIDENAV: {
+    SHOWN_OPTIONS: [SidenavOptions.REPORT_CUSTOM]
   }
 }
