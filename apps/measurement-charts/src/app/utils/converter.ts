3ce572fd1b05ca6ac77cb06458efa7b2841bb910
Lascario Pacheco <zcyonzcyon@gmail.com>
2022-05-11 20:54:03 -0400

    fix: converts from minutes instead of seconds [FRON-2786]

--- apps/measurement-charts/src/app/utils/converter.ts
+++ apps/measurement-charts/src/app/utils/converter.ts
@@ -10,8 +10,8 @@ export const format = (value: number, dataPointTypeId: string) => {
     case DataPointTypes.STEPS:
       return value.toFixed(0)
     case DataPointTypes.SLEEP:
-      const hours = Math.floor(value / 3600)
-      const minutes = Math.floor((value % 3600) / 60)
+      const hours = Math.floor(value / 60)
+      const minutes = Math.floor(value % 60)
       return `${hours > 9 ? hours : `0${hours}`}:${
         minutes > 9 ? minutes : `0${minutes}`
       }` // convert seconds to HH:mm
