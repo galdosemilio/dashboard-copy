4385b08296f4e2615d0750865aa642a3fdff928a
Lascario Pacheco <zcyonzcyon@gmail.com>
2022-05-19 13:39:13 -0400

    fix: hid store in nxtstim [FRON-2800]

--- apps/provider/src/app/config/section/nxtstim/nxtstim.section.config.ts
+++ apps/provider/src/app/config/section/nxtstim/nxtstim.section.config.ts
@@ -1,8 +1,11 @@
-import { MeasurementTabs } from '../consts'
+import { MeasurementTabs, SidenavOptions } from '../consts'
 import { SectionConfigDetails } from '../models/section.details'
 
 export const NXTSTIMSectionConfig: SectionConfigDetails = {
   JOURNAL: {
     HIDDEN_MEASUREMENT_TABS: [MeasurementTabs.FOOD]
+  },
+  SIDENAV: {
+    HIDDEN_OPTIONS: [SidenavOptions.REPORT_CUSTOM, SidenavOptions.STORE]
   }
 }
