4bda1a51acbd89b56ffc9a2509e2d82273980218
Lascario Pacheco <zcyonzcyon@gmail.com>
2022-05-26 10:57:10 -0400

    fix: footer links now react to mala settings [FRON-2797]

--- libs/layout/src/lib/board/footer/footer.component.ts
+++ libs/layout/src/lib/board/footer/footer.component.ts
@@ -1,21 +1,38 @@
-import { Component } from '@angular/core'
-import { ConfigService } from '@coachcare/common/services'
+import { Component, OnInit } from '@angular/core'
+import { ConfigService, ContextService } from '@coachcare/common/services'
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
+import { get } from 'lodash'
 
+@UntilDestroy()
 @Component({
   selector: 'ccr-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {
-  clinicName: string
-  company: string
-  currentYear: number
+export class FooterComponent implements OnInit {
+  public clinicName: string
+  public company: string
+  public currentYear: number
+  public linkTerms?: string
+  public linkPrivacy?: string
+  public linkHipaa?: string
+  public linkMsa?: string
 
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService, private context: ContextService) {
     this.clinicName = config.get(
       'app.layout.footer.clinic',
       'Configure Clinic Name'
     )
     this.company = config.get('app.layout.footer.company', 'CoachCare')
     this.currentYear = new Date().getFullYear()
+  }
+
+  public ngOnInit(): void {
+    this.context.organization$.pipe(untilDestroyed(this)).subscribe((org) => {
+      const links = get(org, 'mala.custom.links') || {}
+      this.linkTerms = links.terms
+      this.linkPrivacy = links.privacy
+      this.linkHipaa = links.hipaa
+      this.linkMsa = links.msa
+    })
   }
 }
