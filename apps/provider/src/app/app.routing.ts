283b4744bb9ec860a25748bd63590202cf660e65
Lascario Pacheco <zcyonzcyon@gmail.com>
2022-06-09 09:02:22 -0400

    fix: disabled preloading for provider site [FRON-2795]

--- apps/provider/src/app/app.routing.ts
+++ apps/provider/src/app/app.routing.ts
@@ -1,10 +1,9 @@
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router'
 
 // LayoutModule routes are also loaded
 const routes: Routes = [{ path: '**', redirectTo: '/' }]
 
 export const AppRoutes = RouterModule.forRoot(routes, {
-  preloadingStrategy: PreloadAllModules,
   paramsInheritanceStrategy: 'always',
   relativeLinkResolution: 'legacy'
 })
