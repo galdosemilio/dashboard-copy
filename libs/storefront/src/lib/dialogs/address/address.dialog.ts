c76f48ad5f0559066738c24548f0b5137171b01a
zhang <zhangxinei@outlook.com>
2022-07-15 10:01:28 -0400

    fix: add state dropdown for us [FRON-2887]
    fix: display full name of state

--- libs/storefront/src/lib/dialogs/address/address.dialog.ts
+++ libs/storefront/src/lib/dialogs/address/address.dialog.ts
@@ -3,12 +3,264 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 import { FormUtils } from '@coachcare/common/shared'
 import { AccountAddress } from '@coachcare/sdk'
-import { UntilDestroy } from '@ngneat/until-destroy'
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
+import { filter } from 'rxjs/operators'
 
 export interface StoreFrontAddressData {
   title: string
   address?: AccountAddress
 }
+
+const stateList = [
+  {
+    displayValue: 'Alabama',
+    value: 'AL'
+  },
+  {
+    displayValue: 'Alaska',
+    value: 'AK'
+  },
+  {
+    displayValue: 'American Samoa',
+    value: 'AS'
+  },
+  {
+    displayValue: 'Arizona',
+    value: 'AZ'
+  },
+  {
+    displayValue: 'Arkansas',
+    value: 'AR'
+  },
+  {
+    displayValue: 'Armed Forces Europe',
+    value: 'AE'
+  },
+  {
+    displayValue: 'Armed Forces Americas',
+    value: 'AA'
+  },
+  {
+    displayValue: 'Armed Forces Pacific',
+    value: 'AP'
+  },
+  {
+    displayValue: 'California',
+    value: 'CA'
+  },
+  {
+    displayValue: 'Colorado',
+    value: 'CO'
+  },
+  {
+    displayValue: 'Connecticut',
+    value: 'CT'
+  },
+  {
+    displayValue: 'Delaware',
+    value: 'DE'
+  },
+  {
+    displayValue: 'District Of Columbia',
+    value: 'DC'
+  },
+  {
+    displayValue: 'Federated States Of Micronesia',
+    value: 'FM'
+  },
+  {
+    displayValue: 'Florida',
+    value: 'FL'
+  },
+  {
+    displayValue: 'Georgia',
+    value: 'GA'
+  },
+  {
+    displayValue: 'Guam',
+    value: 'GU'
+  },
+  {
+    displayValue: 'Hawaii',
+    value: 'HI'
+  },
+  {
+    displayValue: 'Idaho',
+    value: 'ID'
+  },
+  {
+    displayValue: 'Illinois',
+    value: 'IL'
+  },
+  {
+    displayValue: 'Indiana',
+    value: 'IN'
+  },
+  {
+    displayValue: 'Iowa',
+    value: 'IA'
+  },
+  {
+    displayValue: 'Kansas',
+    value: 'KS'
+  },
+  {
+    displayValue: 'Kentucky',
+    value: 'KY'
+  },
+  {
+    displayValue: 'Louisiana',
+    value: 'LA'
+  },
+  {
+    displayValue: 'Maine',
+    value: 'ME'
+  },
+  {
+    displayValue: 'Marshall Islands',
+    value: 'MH'
+  },
+  {
+    displayValue: 'Maryland',
+    value: 'MD'
+  },
+  {
+    displayValue: 'Massachusetts',
+    value: 'MA'
+  },
+  {
+    displayValue: 'Michigan',
+    value: 'MI'
+  },
+  {
+    displayValue: 'Minnesota',
+    value: 'MN'
+  },
+  {
+    displayValue: 'Mississippi',
+    value: 'MS'
+  },
+  {
+    displayValue: 'Missouri',
+    value: 'MO'
+  },
+  {
+    displayValue: 'Montana',
+    value: 'MT'
+  },
+  {
+    displayValue: 'Nebraska',
+    value: 'NE'
+  },
+  {
+    displayValue: 'Nevada',
+    value: 'NV'
+  },
+  {
+    displayValue: 'New Hampshire',
+    value: 'NH'
+  },
+  {
+    displayValue: 'New Jersey',
+    value: 'NJ'
+  },
+  {
+    displayValue: 'New Mexico',
+    value: 'NM'
+  },
+  {
+    displayValue: 'New York',
+    value: 'NY'
+  },
+  {
+    displayValue: 'North Carolina',
+    value: 'NC'
+  },
+  {
+    displayValue: 'North Dakota',
+    value: 'ND'
+  },
+  {
+    displayValue: 'Northern Mariana Islands',
+    value: 'MP'
+  },
+  {
+    displayValue: 'Ohio',
+    value: 'OH'
+  },
+  {
+    displayValue: 'Oklahoma',
+    value: 'OK'
+  },
+  {
+    displayValue: 'Oregon',
+    value: 'OR'
+  },
+  {
+    displayValue: 'Palau',
+    value: 'PW'
+  },
+  {
+    displayValue: 'Pennsylvania',
+    value: 'PA'
+  },
+  {
+    displayValue: 'Puerto Rico',
+    value: 'PR'
+  },
+  {
+    displayValue: 'Rhode Island',
+    value: 'RI'
+  },
+  {
+    displayValue: 'South Carolina',
+    value: 'SC'
+  },
+  {
+    displayValue: 'South Dakota',
+    value: 'SD'
+  },
+  {
+    displayValue: 'Tennessee',
+    value: 'TN'
+  },
+  {
+    displayValue: 'Texas',
+    value: 'TX'
+  },
+  {
+    displayValue: 'Utah',
+    value: 'UT'
+  },
+  {
+    displayValue: 'Vermont',
+    value: 'VT'
+  },
+  {
+    displayValue: 'Virgin Islands',
+    value: 'VI'
+  },
+  {
+    displayValue: 'Virginia',
+    value: 'VA'
+  },
+  {
+    displayValue: 'Washington',
+    value: 'WA'
+  },
+  {
+    displayValue: 'West Virginia',
+    value: 'WV'
+  },
+  {
+    displayValue: 'Wisconsin',
+    value: 'WI'
+  },
+  {
+    displayValue: 'Wyoming',
+    value: 'WY'
+  }
+]
 
 @UntilDestroy()
 @Component({
@@ -20,6 +272,11 @@ export interface StoreFrontAddressData {
 export class StorefrontAddressDialog implements OnInit {
   public title: string
   public form: FormGroup
+  public states = stateList
+
+  get isUnitedStates() {
+    return this.form?.value.country === 'US'
+  }
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: StoreFrontAddressData,
@@ -34,6 +291,24 @@ export class StorefrontAddressDialog implements OnInit {
     if (this.data.address) {
       this.patchValues(this.data.address)
     }
+
+    this.form
+      .get('country')
+      .valueChanges.pipe(
+        untilDestroyed(this),
+        filter((country) => country === 'US')
+      )
+      .subscribe(() => {
+        if (
+          !this.states
+            .map((state) => state.value)
+            .includes(this.form.value.stateProvince)
+        ) {
+          this.form.patchValue({
+            stateProvince: ''
+          })
+        }
+      })
   }
 
   private patchValues(address: AccountAddress) {
