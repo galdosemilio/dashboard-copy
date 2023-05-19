3e20752790e13191e3526b789189b36e50a6ec66
Alex Pereira <ja.pereiragt@gmail.com>
2023-05-19 16:09:26 -0600

    fix: remove register new company link on Be Slim login page

--- apps/admin/src/app/pages/config/be-slim.section.config.ts
+++ apps/admin/src/app/pages/config/be-slim.section.config.ts
@@ -3,5 +3,9 @@ import { SectionConfigDetails } from './section.config'
 export const BeSlimSectionConfig: SectionConfigDetails = {
   CHECKOUT: {
     AUTOMATIC_SHOPIFY_REDIRECT: true
+  },
+  LOGIN: {
+    SHOW_REGISTER_NEW_COMPANY: false,
+    USE_COOKIE_BASED_SESSION: true
   }
 }
